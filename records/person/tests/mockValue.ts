8a22d4c63ca4e5ecad94dd3a2bd5f732312511d0
chimny <aleks713@gmail.com>
2023-01-17 15:12:17 +0100

    working with tests

---
+++ records/person/tests/mockValue.ts
@@ -0,0 +1,17 @@
+
+
+export const mockValue = [[
+    {
+        id: '123',
+        name: 'John',
+        surName: 'Doe',
+    },
+    {
+        id: '456',
+        name: 'Jane',
+        surName: 'Doe',
+    }
+], []];
+
+
+
--- records/person/tests/personInsert.test.ts
+++ records/person/tests/personInsert.test.ts
@@ -1,26 +1,13 @@
 import {PersonRecord} from "../person.record";
 import {pool} from "../../../utils/db";
+import { mockValue } from "./mockValue";
+
 
 
 jest.mock('../../../utils/db', () => {
     return {
         pool: {
-            execute: jest.fn().mockImplementation(() => Promise.resolve([[
-                {
-                    personId: '123',
-                    name: 'John',
-                    surName: 'Doe',
-                    position: 'Manager',
-                    salary: 5000
-                },
-                {
-                    personId: '456',
-                    name: 'Jane',
-                    surName: 'Doe',
-                    position: 'Assistant',
-                    salary: 4000
-                }
-            ], []]))
+            execute: jest.fn().mockImplementation(() => Promise.resolve(mockValue))
         }
     };
 });
