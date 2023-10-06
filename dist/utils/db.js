02b162daa3c87bff22407cef0aaa0345435bba79
chimny <aleks713@gmail.com>
2023-10-06 14:42:55 +0200

    m

--- dist/utils/db.js
+++ dist/utils/db.js
@@ -2,22 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.pool = void 0;
 const promise_1 = require("mysql2/promise");
-exports.pool = (0, promise_1.createPool)({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    namedPlaceholders: true,
-    decimalNumbers: true,
-});
 // export const pool = createPool({
-//     host: 'sql7.freesqldatabase.com',
-//     user: 'sql7626446',
-//     database: 'sql7626446',
-//     password: 'epH4mhwXbW',
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     database: process.env.DB_DATABASE,
+//     password: process.env.DB_PASSWORD,
 //     namedPlaceholders: true,
 //     decimalNumbers: true,
 // });
+exports.pool = (0, promise_1.createPool)({
+    host: 'sql7.freesqldatabase.com',
+    user: 'sql7626446',
+    database: 'sql7626446',
+    password: 'epH4mhwXbW',
+    namedPlaceholders: true,
+    decimalNumbers: true,
+});
 // below pool for localhost connection
 //  export const pool = createPool({
 //     host: 'localhost',
--- utils/db.ts
+++ utils/db.ts
@@ -1,22 +1,22 @@
 import {createPool} from "mysql2/promise";
 
-export const pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    namedPlaceholders: true,
-    decimalNumbers: true,
-});
-
 // export const pool = createPool({
-//     host: 'sql7.freesqldatabase.com',
-//     user: 'sql7626446',
-//     database: 'sql7626446',
-//     password: 'epH4mhwXbW',
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     database: process.env.DB_DATABASE,
+//     password: process.env.DB_PASSWORD,
 //     namedPlaceholders: true,
 //     decimalNumbers: true,
 // });
+
+export const pool = createPool({
+    host: 'sql7.freesqldatabase.com',
+    user: 'sql7626446',
+    database: 'sql7626446',
+    password: 'epH4mhwXbW',
+    namedPlaceholders: true,
+    decimalNumbers: true,
+});
 
 
  // below pool for localhost connection
